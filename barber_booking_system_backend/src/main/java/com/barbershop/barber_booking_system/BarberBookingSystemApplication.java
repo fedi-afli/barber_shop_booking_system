a27d3ed6c74c1fb837d1b2a7919770029eeced3d
fedi-afli <f3diafli@gmail.com>
2026-06-14 02:05:25 +0100

    initial commit

---
+++ barber_booking_system_backend/src/main/java/com/barbershop/barber_booking_system/BarberBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.barbershop.barber_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberBookingSystemApplication.class, args);
+	}
+
+}
